cbbc1070c5294b2154adea50e66d2081ab16b19c
HollowDude <madafakn@gmail.com>
2024-10-17 16:50:47 -0500

    Update settings.py

--- Vinilos/settings.py
+++ Vinilos/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!mo9*&90ady7hy5n+drbb1bimijp7vs9*+@-4-mjyu+w%w^ro@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vinilostudios.me', 'localhost']
 
 
 # Application definition
