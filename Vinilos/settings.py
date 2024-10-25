6e07abd0549e7474298ce2f8444cd51e80c73221
HollowDude <madafakn@gmail.com>
2024-10-25 12:20:30 -0500

    Update settings.py

--- Vinilos/settings.py
+++ Vinilos/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'authenticate.middleware.JWTAuthCookieMiddleware',
+    'authenticate.middleware.SameSiteSecureCookieMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
