d8b75153ef71a55152a370d120c4ae5f1e94c67d
HollowDude <madafakn@gmail.com>
2024-10-25 20:57:34 -0500

    Update urls.py

--- Vinilos/urls.py
+++ Vinilos/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('api/auth/', include('dj_rest_auth.urls')), 
     path('admin/', admin.site.urls),
     path('api/piercs/', include('piercings.urls')),
-    path('api/tatts/', include('tattoos.urls'))
+    path('api/tatts/', include('tattoos.urls')),
+    path('keep_alive/', views.keep_alive, name='keep_alive'),
 ]
