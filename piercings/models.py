945d660ea7a1cd6c88e50bccff80b1545b49d44a
HollowDude <madafakn@gmail.com>
2024-10-26 23:35:09 -0500

    Base64

--- piercings/models.py
+++ piercings/models.py
@@ -5,6 +5,6 @@ class piercings(models.Model) :
     name = models.CharField(max_length = 255, default = "No name")
     description = models.CharField(max_length = 255, default = "No description")
     price = models.IntegerField(default = int(1))
-    photo = models.BinaryField()
+    photo = models.BinaryField(blank=True, null=True)
     def __str__(self):
         return self.name
