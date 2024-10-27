87d2e4736e43209478793324efbc97211054e9dc
HollowDude <madafakn@gmail.com>
2024-10-26 22:59:03 -0500

    Update models.py

--- piercings/models.py
+++ piercings/models.py
@@ -6,5 +6,3 @@ class piercings(models.Model) :
     description = models.CharField(max_length = 255, default = "No description")
     price = models.IntegerField(default = int(1))
     photo = models.BinaryField(blank=True, null=True)
-    def __str__(self):
-        return self.name
