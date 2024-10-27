75b4b6f241f660e5f202c7f81e252f59428b0cfd
HollowDude <madafakn@gmail.com>
2024-10-26 22:52:50 -0500

    Update serializers.py

--- piercings/serializers.py
+++ piercings/serializers.py
@@ -2,12 +2,12 @@ import base64
 from .models import piercings
 from rest_framework import serializers
 class PiercingsSerializer(serializers.ModelSerializer):
-    photo = serializers.SerializerMethodField()
+    photos = serializers.SerializerMethodField()
     class Meta:
         model = piercings
         fields = '__all__'
 
     def get_imagen(self, obj):
-        if obj.photo:
-            return base64.b64encode(obj.photo).decode('utf-8')
+        if obj.photos:
+            return base64.b64encode(obj.photos).decode('utf-8')
         return None
