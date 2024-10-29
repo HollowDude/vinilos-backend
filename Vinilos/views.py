f91bad9ffc2c19c931746650630b1ad434dcb112
HollowDude <madafakn@gmail.com>
2024-10-29 05:20:58 -0500

    Arregle la respouesta2

--- Vinilos/views.py
+++ Vinilos/views.py
@@ -14,8 +14,13 @@ def is_auth(request):
     USERNAME = request.data.get('username')
     PASSWORD = request.data.get('password')
 
+    print(USERNAME)
+    print(PASSWORD)
+    print(os.getenv('USERNAME'))
+    print(os.getenv('PASSWORD'))
+
     if USERNAME == os.getenv('USERNAME') and PASSWORD == os.getenv('PASSWORD'):
-        return JsonResponse({'Bacano': 'Te autenticaste bacano'}, 200)
+        return JsonResponse({'Bacano': 'Te autenticaste bacano'}, status.HTTP_200_OK)
     return JsonResponse({'Uh, problema':'No introdujiste bien un dato bro'}, status = status.HTTP_401_UNAUTHORIZED)
 
 
