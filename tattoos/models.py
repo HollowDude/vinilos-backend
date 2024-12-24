2e9af91f53353320a21bcc641aba63a4dcddd3ba
HollowDude <madafakn@gmail.com>
2024-12-23 23:49:10 -0500

    Cambios de lo de las img base 64 2

--- tattoos/models.py
+++ tattoos/models.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 from django.db import models
 
+def file_location(instance, filename, **kwargs):
+    file_path = f"photo/{instance.title}-{filename}"
+    return file_path
+
 class tattoos(models.Model):
     name = models.CharField(max_length=255, default="No definido")
     description = models.CharField(max_length = 255, default="No hay descripcion")
-    photo_b64 = models.TextField()
+    photo = models.ImageField(upload_to=file_location, null=False, blank=True)
     date = models.DateField(default = datetime.now())
     price = models.IntegerField(default=int(1))
--- tattoos/serializers.py
+++ tattoos/serializers.py
@@ -1,15 +1,12 @@
 import base64
 from .models import tattoos
 from rest_framework import serializers
+from drf_extra_fields.fields import Base64ImageField
+
+
 class TattooSerializer(serializers.ModelSerializer):
-    photo = serializers.ImageField(write_only = True)
+    photo = Base64ImageField(required=False)
 
     class Meta:
         model = tattoos
-        fields = ('name','description', 'photo', 'photo_b64', 'date', 'price')
-
-    def create(self, validated_data):
-        photo = validated_data.pop('photo')
-        photo_b64 = base64.encode_base64(photo.read()).decode('utf-8')
-        validated_data['photo_b64'] = photo_b64
-        return photo_b64.objects.create(**validated_data)
+        fields = ('name','description', 'photo_b64', 'date', 'price')
